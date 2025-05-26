2a044b1303ee6a6424f504999f40c29e233d288b
Kanika Singhal <kanikasin216@gmail.com>
2025-05-26 20:40:05 +0530

    modified structure of project

---
+++ server/index.js
@@ -0,0 +1,31 @@
+
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB= require("./db/connectDB.js")
+const userRoutes = require("./routes/userRoutes");
+const gigRoutes = require("./routes/gigRoutes");
+// const proposalRoutes = require("./routes/proposalRoutes");
+const eventRoutes = require("./routes/eventRoutes");
+// const chatRoutes = require("./routes/chatRoutes");
+
+dotenv.config();
+const app = express();
+connectDB();
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use("/api/users", userRoutes);
+app.use("/api/gigs", gigRoutes);
+// app.use("/api/proposals", proposalRoutes);
+app.use("/api/events", eventRoutes);
+// app.use("/api/chat", chatRoutes);
+
+const PORT= process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
