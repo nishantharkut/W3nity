177515474b78f8cb1d97900357224d23e481e22b
Nishant H. <156298209+nishantharkut@users.noreply.github.com>
2025-05-30 03:03:19 +0530

    Update Ticket.d.ts

--- server/types/Ticket.d.ts
+++ server/types/Ticket.d.ts
@@ -1,4 +1,15 @@
 declare module "../models/ticket.js" {
-  const Ticket: any;
+  import mongoose from "mongoose";
+
+  interface TicketDocument extends mongoose.Document {
+    wallet: string;
+    tokenId: number;
+    tokenURI: string;
+    transactionHash: string;
+    eventId?: mongoose.Types.ObjectId;
+    mintedAt?: Date;
+  }
+
+  const Ticket: mongoose.Model<TicketDocument>;
   export default Ticket;
 }
