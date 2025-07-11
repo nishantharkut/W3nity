787273299332f4e2ac02762464fa64bde31590ed
Nishant H. <156298209+nishantharkut@users.noreply.github.com>
2025-07-11 18:57:51 +0530

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,21 +1,44 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react-swc";
+// import path from "path";
+
+// // https://vitejs.dev/config/
+// export default defineConfig(({ mode }) => ({
+//   server: {
+//     host: "::",
+//     port: 8080,
+//   },
+//   plugins: [
+//     react(),
+//     mode === 'development' &&
+//     componentTagger(), 
+//   ].filter(Boolean),
+//   resolve: {
+//     alias: {
+//       '@': path.resolve(__dirname, 'src'),
+//     },
+//   },
+// }));
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import { fileURLToPath } from "url";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default defineConfig({
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(), 
-  ].filter(Boolean),
+  ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      "@": path.resolve(__dirname, "src"),
     },
   },
-}));
+});
